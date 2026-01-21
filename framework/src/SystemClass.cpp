d6c4e1822a3d9d1a0ee8d246eebad0e928592fa8
Luis Dutra <luisvinicius99@gmail.com>
2026-01-20 21:52:09 -0300

    add primeiras funcoes do framework

--- framework/src/SystemClass.cpp
+++ framework/src/SystemClass.cpp
@@ -1,25 +1,62 @@
 #include "SystemClass.hpp"
 
-SystemClass::SystemClass(){
-
+SystemClass::SystemClass() {
+  m_Input = 0;
+  m_Application = 0;
 }
 
-SystemClass::SystemClass(const SystemClass& other){
-    
+SystemClass::SystemClass(const SystemClass &other) {}
+
+SystemClass::~SystemClass() {}
+
+bool SystemClass::Initialize() {
+  int screenWidth, screenHeight;
+  bool result;
+
+  m_Input = new InputClass;
+
+  m_Input->Initialize();
+
+  screenWidth = 0;
+  screenHeight = 0;
+
+  result = InitializeWindow(screenWidth, screenHeight);
+
+  if (result) {
+    return false;
+  }
 }
 
-SystemClass::~SystemClass(){
+void SystemClass::Shutdown() { 
+  if(m_Application){
+    m_Application->Shutdown();
+    delete m_Application;
+    m_Application = 0;
+  }
 
+  ShutdownWindow();
+
+  if(m_Input){
+    delete m_Input;
+    m_Input = 0;
+  }
+
+  return;
 }
 
-bool SystemClass::Initialize(){
-    return true;
-}
+void SystemClass::Frame() { 
+  bool done, result;
 
-void SystemClass::Shutdown(){
-    return;
-}
+  done = false;
 
-void SystemClass::Frame(){
-    return;
-}
+  while(!done){
+    ReadInput();
+
+    result = m_Application->Frame(m_Input);
+    if(!result){
+      done = true;
+    }
+  }
+
+  return;
+ }
--- framework/src/SystemClass.hpp
+++ framework/src/SystemClass.hpp
@@ -1,17 +1,33 @@
 #ifndef _SYSTEMCLASS_H_
 #define _SYSTEMCLASS_H_
 
+#include <unistd.h>
+
+#include "ApplicationClass.hpp"
+#include <X11/Xlib.h>
+
+
 class SystemClass {
-    public:
-        SystemClass();
-        SystemClass(const SystemClass&);
-        ~SystemClass();
+public:
+  SystemClass();
+  SystemClass(const SystemClass &);
+  ~SystemClass();
 
-        bool Initialize();
-        void Shutdown();
-        void Frame();
+  bool Initialize();
+  void Shutdown();
+  void Frame();
 
-    private:
+private:
+  bool InitializeWindow(int &, int &);
+  void ShutdownWindow();
+  void ReadInput();
+
+private:
+  ApplicationClass *m_Application;
+  InputClass *m_Input;
+  Display *m_videoDisplay;
+  Window m_hwnd;
+  GLXContext m_renderingContext;
 };
 
 #endif
