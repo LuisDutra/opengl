a5c58563d542d756c738f34b3912ea1483f6c20f
Luis Dutra <luisvinicius99@gmail.com>
2026-01-26 20:09:22 -0300

    Finalize InputClass

--- framework/src/InputClass.cpp
+++ framework/src/InputClass.cpp
@@ -1,13 +1,36 @@
 #include "InputClass.hpp"
 
-InputClass::InputClass(){
+InputClass::InputClass() {}
 
+InputClass::InputClass(const InputClass &other) {}
+
+InputClass::~InputClass() {}
+
+void InputClass::Initialize() {
+  int i;
+
+  // Initialize the keyboard state.
+  for (i = 0; i < 256; i++) {
+    m_keyboardState[i] = false;
+  }
+
+  return;
 }
 
-InputClass::InputClass(const InputClass& other){
+void InputClass::KeyDown(int keySymbol) {
+  if (keySymbol == 65307) {
+    m_keyboardState[KEY_ESCAPE] = true;
+  }
 
+  return;
 }
 
-InputClass::~InputClass(){
+void InputClass::KeyUp(int keySymbol) {
+  if (keySymbol == 65307) {
+    m_keyboardState[KEY_ESCAPE] = false;
+  }
 
+  return;
 }
+
+bool InputClass::IsEscapePressed() { return m_keyboardState[KEY_ESCAPE]; }
--- framework/src/InputClass.hpp
+++ framework/src/InputClass.hpp
@@ -1,13 +1,23 @@
 #ifndef _INPUTCLASS_H_
 #define _INPUTCLASS_H_
 
-class InputClass{
-    public:
-     InputClass();
-     InputClass(const InputClass&);
-     ~InputClass();
+const int KEY_ESCAPE = 0;
 
-    private:
+class InputClass {
+public:
+  InputClass();
+  InputClass(const InputClass &);
+  ~InputClass();
+
+  void Initialize();
+
+  void KeyDown(int);
+  void KeyUp(int);
+
+  bool IsEscapePressed();
+
+private:
+  bool m_keyboardState[256];
 };
 
 #endif
